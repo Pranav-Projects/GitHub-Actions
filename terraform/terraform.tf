3dce34df1b4171f5f74ab9b3e76dcef178dfd71f
Pranav Swaroop <115005957+Pranav10ss@users.noreply.github.com>
2025-05-05 19:26:31 +0900

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    profile = "tfstate"
+    bucket  = "terraform-state-backend-bucket1 "
+    key     = "state.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
